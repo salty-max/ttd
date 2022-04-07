5671b2c81258892e98dad1b0f67ead6c3d6f5061
salty-max <max@jellycat.fr>
2022-04-07 18:43:42 +0200

    adds notifications + item drag

--- README.md
+++ README.md
@@ -17,7 +17,9 @@ $ cargo run SAMPLE.txt
 |Keys|Description|
 |---|---|
 |<kbd>w</kbd>, <kbd>z</kbd>|Move up|
+|<kbd>Shift</kbd> + <kbd>w</kbd>|Drag item up|
 |<kbd>s</kbd>|Move down|
+|<kbd>Shift</kbd> + <kbd>s</kbd>|Drag item down|
 |<kbd>Return</kbd>|Toggle selected element's state|
-|<kbd>Tab</kbd>|Switch between tabs|
+|<kbd>Tab</kbd>|Switch between panels|
 |<kbd>q</kbd>|Quit|
--- src/main.rs
+++ src/main.rs
@@ -8,8 +8,7 @@
 use std::{
     env,
     fs::File,
-    io::{self, BufRead, BufReader, Write},
-    path::Path,
+    io::{self, BufRead, BufReader, ErrorKind, Write},
     process::{self},
 };
 
@@ -26,6 +25,19 @@ fn move_up(selected: &mut ID) {
 }
 fn move_down(selected: &mut ID, list: &[String]) {
     if *selected + 1 < list.len() {
+        *selected += 1;
+    }
+}
+
+fn drag_up(list: &mut Vec<String>, selected: &mut ID) {
+    if *selected > 0 {
+        list.swap(*selected, *selected - 1);
+        *selected -= 1;
+    }
+}
+fn drag_down(list: &mut Vec<String>, selected: &mut ID) {
+    if *selected + 1 < list.len() {
+        list.swap(*selected, *selected + 1);
         *selected += 1;
     }
 }
@@ -55,22 +67,10 @@ fn parse_item(line: &str) -> Option<(Status, &str)> {
 }
 
 fn load_state(todos: &mut Vec<String>, dones: &mut Vec<String>, file_path: &str) -> io::Result<()> {
-    let file = {
-        if Path::new(file_path).exists() {
-            File::open(file_path)?
-        } else {
-            File::options()
-                .read(true)
-                .write(true)
-                .create_new(true)
-                .open(file_path)?
-        }
-    };
+    let file = File::open(file_path)?;
 
-    let buffer = BufReader::new(file);
-
-    for (index, line) in buffer.lines().enumerate() {
-        match parse_item(&line.unwrap()) {
+    for (index, line) in BufReader::new(file).lines().enumerate() {
+        match parse_item(&line?) {
             Some((Status::Todo, label)) => todos.push(String::from(label)),
             Some((Status::Done, label)) => dones.push(String::from(label)),
             None => {
@@ -115,6 +115,18 @@ fn main() -> std::io::Result<()> {
     let mut selected_done: ID = 0;
 
     load_state(&mut todos, &mut dones, &file_path)?;
+    let mut notification: String;
+
+    match load_state(&mut todos, &mut dones, &file_path) {
+        Ok(()) => notification = format!("Loaded file {}", file_path),
+        Err(e) => {
+            if e.kind() == ErrorKind::NotFound {
+                notification = format!("New file {}", file_path);
+            } else {
+                panic!("Could not load state from file `{}`: {:?}", file_path, e);
+            }
+        }
+    }
 
     initscr();
     noecho();
@@ -138,48 +150,60 @@ fn main() -> std::io::Result<()> {
         let mut window_h = 0;
         getmaxyx(stdscr(), &mut window_h, &mut window_w);
 
-        ui.begin(Vec2::zero(), LayoutDir::Horizontal);
+        ui.begin(Vec2::zero(), LayoutDir::Vertical);
 
-        ui.begin_layout(LayoutDir::Vertical);
+        ui.label_fixed_width(&notification, REGULAR_PAIR, window_w);
+        notification.clear();
+        ui.label_fixed_width("", REGULAR_PAIR, window_w);
 
-        let todo_header_color = if focus == Status::Todo {
-            HIGHLIGHT_PAIR
-        } else {
-            REGULAR_PAIR
-        };
+        ui.begin_layout(LayoutDir::Horizontal);
 
-        ui.label_fixed_width(" TODO ", todo_header_color, window_w / 2);
-        ui.label_fixed_width("------------", REGULAR_PAIR, window_w / 2);
+        {
+            ui.begin_layout(LayoutDir::Vertical);
 
-        for (index, todo) in todos.iter().enumerate() {
-            let pair = if index == selected_todo && focus == Status::Todo {
+            let todo_header_color = if focus == Status::Todo {
                 HIGHLIGHT_PAIR
             } else {
                 REGULAR_PAIR
             };
-            ui.label_fixed_width(&format!("- [ ] {}", todo), pair, window_w / 2);
+
+            ui.label_fixed_width(" TODO ", todo_header_color, window_w / 2);
+            ui.label_fixed_width("------------", REGULAR_PAIR, window_w / 2);
+
+            for (index, todo) in todos.iter().enumerate() {
+                let pair = if index == selected_todo && focus == Status::Todo {
+                    HIGHLIGHT_PAIR
+                } else {
+                    REGULAR_PAIR
+                };
+                ui.label_fixed_width(&format!("- [ ] {}", todo), pair, window_w / 2);
+            }
+
+            ui.end_layout();
         }
 
-        ui.end_layout();
+        {
+            ui.begin_layout(LayoutDir::Vertical);
 
-        ui.begin_layout(LayoutDir::Vertical);
-
-        let done_header_color = if focus == Status::Done {
-            HIGHLIGHT_PAIR
-        } else {
-            REGULAR_PAIR
-        };
-
-        ui.label_fixed_width(" DONE ", done_header_color, window_w / 2);
-        ui.label_fixed_width("------------", REGULAR_PAIR, window_w / 2);
-
-        for (index, done) in dones.iter().enumerate() {
-            let pair = if index == selected_done && focus == Status::Done {
+            let done_header_color = if focus == Status::Done {
                 HIGHLIGHT_PAIR
             } else {
                 REGULAR_PAIR
             };
-            ui.label_fixed_width(&format!("- [x] {}", done), pair, window_w / 2);
+
+            ui.label_fixed_width(" DONE ", done_header_color, window_w / 2);
+            ui.label_fixed_width("------------", REGULAR_PAIR, window_w / 2);
+
+            for (index, done) in dones.iter().enumerate() {
+                let pair = if index == selected_done && focus == Status::Done {
+                    HIGHLIGHT_PAIR
+                } else {
+                    REGULAR_PAIR
+                };
+                ui.label_fixed_width(&format!("- [x] {}", done), pair, window_w / 2);
+            }
+
+            ui.end_layout();
         }
 
         ui.end_layout();
@@ -195,33 +219,39 @@ fn main() -> std::io::Result<()> {
                 Status::Todo => move_up(&mut selected_todo),
                 Status::Done => move_up(&mut selected_done),
             },
+            'W' | 'Z' => match focus {
+                Status::Todo => drag_up(&mut todos, &mut selected_todo),
+                Status::Done => drag_up(&mut dones, &mut selected_done),
+            },
             's' => match focus {
                 Status::Todo => move_down(&mut selected_todo, &todos),
                 Status::Done => move_down(&mut selected_done, &dones),
             },
+            'S' => match focus {
+                Status::Todo => drag_down(&mut todos, &mut selected_todo),
+                Status::Done => drag_down(&mut dones, &mut selected_done),
+            },
             '\n' => match focus {
-                Status::Todo => list_transfer(&mut todos, &mut dones, &mut selected_todo),
-                Status::Done => list_transfer(&mut dones, &mut todos, &mut selected_done),
+                Status::Todo => {
+                    list_transfer(&mut todos, &mut dones, &mut selected_todo);
+                    notification.push_str("DONE!");
+                }
+                Status::Done => {
+                    list_transfer(&mut dones, &mut todos, &mut selected_done);
+                    notification.push_str("Not done yet...");
+                }
             },
             '\t' => {
                 focus = focus.toggle();
             }
-            // 'S' => {
-            //     let mut file = File::create("TODO.txt")?;
-            //     for todo in todos.iter() {
-            //         writeln!(file, "TODO: {}", todo)?;
-            //     }
-            //     for done in dones.iter() {
-            //         writeln!(file, "DONE: {}", done)?;
-            //     }
-            // }
             _ => {}
         }
     }
 
-    save_state(&todos, &dones, &file_path)?;
-
     endwin();
+
+    save_state(&todos, &dones, &file_path)?;
+    println!("Saved state to {}", file_path);
 
     Ok(())
 }
